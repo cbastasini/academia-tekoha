761cd80301e7a4d0d4d2df450db16957fbab1133
Caio Bastasini <1851494+cbastasini@users.noreply.github.com>
2026-01-15 15:03:59 -0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,27 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://academiatekoha.com.br',
+  output: 'static',
   integrations: [
     sitemap({
-      customPages: ['https://academiatekoha.com.br/blog'],
+      customPages: [
+        'https://academiatekoha.com.br/',
+        'https://academiatekoha.com.br/#modalidades',
+        'https://academiatekoha.com.br/#beneficios',
+        'https://academiatekoha.com.br/#filosofia',
+        'https://academiatekoha.com.br/#depoimentos',
+        'https://academiatekoha.com.br/#localizacao',
+      ],
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
+      filter: (page) => !page.includes('/blog/'),
     })
   ],
   build: {
-    inlineStylesheets: 'auto'
+    inlineStylesheets: 'auto',
+    format: 'directory'
   },
-  compressHTML: true
+  compressHTML: true,
+  trailingSlash: 'ignore'
 });
